08118aded254668dba925a98ee322c107923e60b
Arda Kaya <ardakya99@hotmail.com>
2023-09-25 16:27:00 +0300

    connect database and datas completed

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const app = express();
+const Post = require('./models/Post');
 
-const blog = {
-  id: 1,
-  title: 'Blog title',
-  description: 'Blog description',
-};
 
-app.get('/', (req, res) => {
-  res.send(blog);
+// connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', async(req, res) => {
+  const posts = await Post.find({});
+  console.log(posts);
+  res.render('index',{
+    posts
+  });
 });
+app.get('/about', (req, res) => {
+  res.render("about")
+});
+
+app.get('/add_post', (req, res) => {
+  res.render("add_post")
+});
+app.get('/post', (req, res) => {
+  res.render("post")
+});
+
+
+// FORM POST REQUEST
+
+app.post('/posts', async(req,res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+
+
 
 const port = 3001;
 
